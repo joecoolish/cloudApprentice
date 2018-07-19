7d0d24667d6ffa68c39fe2d3cc3ed07a392bd894
Steve <strong_stephen@bah.com>
2018-07-19 10:18:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Cloud Apprentice', { horizontalLayout: 'full' })
+  )
+);
