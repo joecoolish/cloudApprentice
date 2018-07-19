c3253532b3b02c05660ba90e39f8f1a965861f61
Steve <strong_stephen@bah.com>
2018-07-19 11:53:29 -0400

    added commander

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+//  https://www.sitepoint.com/javascript-command-line-interface-cli-node-js/
+
+
 const chalk       = require('chalk');
 const clear       = require('clear');
 const figlet      = require('figlet');
@@ -10,3 +13,19 @@ console.log(
     figlet.textSync('Cloud Apprentice', { horizontalLayout: 'full' })
   )
 );
+
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+ 
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
