7b39375c4499ef32a9c057e29db600333114e953
Steve <strong_stephen@bah.com>
2018-07-19 21:29:39 -0400

    more things

--- cmd/azure.js
+++ cmd/azure.js
@@ -1,8 +1,19 @@
 
 const Azure = require('azure');
-const MsRest = require('ms-rest-azure');
+
+var util = require('util');
+var path = require('path');
+var async = require('async');
+var msRestAzure = require('ms-rest-azure');
+var ComputeManagementClient = require('azure-arm-compute');
+var StorageManagementClient = require('azure-arm-storage');
+var NetworkManagementClient = require('azure-arm-network');
+var ResourceManagementClient = require('azure-arm-resource').ResourceManagementClient;
+var SubscriptionManagementClient = require('azure-arm-resource').SubscriptionClient;
 
 const log = console.log;
+
+var resourceClient, computeClient, storageClient, networkClient;
 
 function azurelogin(username, password) {
   log('calling azurelogin ', password);
@@ -14,18 +25,38 @@ function azurelogin(username, password) {
   const pass =  password || '';
 
 
-  MsRest.loginWithUsernamePassword(user, pass, (err, credentials) => {
+  msRestAzure.loginWithUsernamePassword(user, pass, (err, credentials) => {
     if (err) throw err;
 
     log('calling login complete');
 
-    let storageClient = Azure.createStorageManagementClient(credentials, 'subscription-id');
+    let subscriptionId = 'subscription-id';
 
-    log(storageClient);
+    resourceClient = new ResourceManagementClient(credentials, subscriptionId);
+    computeClient = new ComputeManagementClient(credentials, subscriptionId);
+    storageClient = new StorageManagementClient(credentials, subscriptionId);
+    networkClient = new NetworkManagementClient(credentials, subscriptionId);
 
+
+    log(computeClient);
+
+    log('\n>>>>>>>Start of Task5: List all vms under the current subscription.');
+    computeClient.virtualMachines.listAll((err, result) => {
+      if (err) {
+        log(util.format('\n???????Error in Task5: while listing all the vms under ' + 
+          'the current subscription:\n%s', util.inspect(err, { depth: null })));
+        //callback(err);
+      } else {
+        log(util.format('\n######End of Task5: List all the vms under the current ' + 
+          'subscription is successful.\n%s', util.inspect(result, { depth: null })));
+        //callback(null, result);
+      }
+    }
     // ..use the client instance to manage service resources.
-  });
+  }
 }
+
+
 
 
 exports.login = azurelogin;
